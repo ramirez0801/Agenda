bd746ad0b3723ae42fc1a42bbd0db9b650490999
ramirez0801 <ramirez.erg@hotmail.com>
2025-10-21 15:12:34 -0600

    Add: File and code for program logic

---
+++ Agenda.Negocio/ContactService.cs
@@ -0,0 +1,35 @@
+﻿using Agenda.Datos;
+using Agenda.Entidades;
+
+namespace Agenda.Negocio
+{
+    public class ContactService
+    {
+        private readonly IRepository<Contacto> _repository;
+        public ContactService(IRepository<Contacto> repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddContact(string name, string email, string phone)
+        {
+            var contact = new Contacto
+            {
+                Name = name,
+                Email = email,
+                Phone = phone
+            };
+
+            _repository.Add(contact);
+        }
+
+        public void UpdateContact(Contacto contact) => _repository.Update(contact);
+
+        public void DeleteContact(int id) => _repository.Delete(id);
+
+        public Contacto GetContact(int id) => _repository.GetById(id);
+
+        public IEnumerable<Contacto> GetAll() => _repository.GetAll();
+
+    }
+}
